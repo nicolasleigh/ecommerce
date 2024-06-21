92f1b844847726a464a33c6631be96c6186e53ce
Nicolas Leigh <linze201@gmail.com>
2024-06-21 22:44:44 +0800

    create Sidebar styles

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 export default function MainLayout() {
   return (
-    <div>
-      MainLayout <Outlet />{" "}
+    <div className='bg-[#cdcae9] w-full min-h-screen'>
+      <Header />
+      <Sidebar />
+      <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all'>
+        <Outlet />
+      </div>
     </div>
   );
 }
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,3 +1,56 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { getNav } from "../navigation";
+import { BiLogOutCircle } from "react-icons/bi";
+
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const [allNav, setAllNav] = useState([]);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const navs = getNav("admin");
+    setAllNav(navs);
+  }, []);
+
+  return (
+    <div>
+      <div></div>
+      <div
+        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}
+      >
+        <div className='h-[70px] flex justify-center items-center'>
+          <Link to='/' className='w-[180px] h-[50px]'>
+            <img src='http://localhost:5173/logo.png' alt='logo image' className='w-full h-full' />
+          </Link>
+        </div>
+        <div className='px-[16px]'>
+          <ul>
+            {allNav.map((nav, index) => (
+              <li key={nav.title}>
+                <Link
+                  to={nav.path}
+                  className={`${
+                    pathname === nav.path
+                      ? "bg-blue-600 shadow-indigo-500/50 text-white duration-500"
+                      : "text-[#030811] font-bold duration-200"
+                  } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1`}
+                >
+                  <span>{nav.icon}</span>
+                  <span>{nav.title}</span>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <button className='text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1'>
+                <span>
+                  <BiLogOutCircle />
+                </span>
+                <span>Logout</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
