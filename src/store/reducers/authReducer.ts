520b9e00c2452acc689af1357722fefe1e1f37f9
Nicolas Leigh <linze201@gmail.com>
2024-06-21 21:43:19 +0800

    add routes manage

--- src/store/reducers/authReducer.ts
+++ src/store/reducers/authReducer.ts
@@ -4,6 +4,7 @@ import { api } from "../../api";
 export const admin_login = createAsyncThunk("auth/admin_login", async (info, { rejectWithValue, fulfillWithValue }) => {
   try {
     const { data } = await api.post("/admin-login", info, { withCredentials: true });
+    localStorage.setItem("accessToken", data.token);
     return fulfillWithValue(data);
   } catch (error) {
     return rejectWithValue(error.response.data);
