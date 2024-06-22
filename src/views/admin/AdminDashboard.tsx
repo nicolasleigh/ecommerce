34e26a7cf122c4f8d36fd9566dc7c81efda495d9
Nicolas Leigh <linze201@gmail.com>
2024-06-22 17:01:56 +0800

    create AdminDashboard chart

--- src/views/admin/AdminDashboard.tsx
+++ src/views/admin/AdminDashboard.tsx
@@ -1,3 +1,122 @@
+import { FaUser } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import Chart from "react-apexcharts";
+import { MdCurrencyExchange, MdProductionQuantityLimits } from "react-icons/md";
+
 export default function AdminDashboard() {
-  return <div>AdminDashboard</div>;
+  const state = {
+    series: [
+      {
+        name: "Orders",
+        data: [23, 34, 45, 56, 76, 34, 23, 76, 87, 78, 35, 74],
+      },
+      {
+        name: "Revenue",
+        data: [67, 39, 45, 56, 90, 56, 23, 56, 87, 78, 76, 12],
+      },
+      {
+        name: "Sellers",
+        data: [34, 39, 45, 56, 50, 65, 33, 56, 74, 82, 19, 29],
+      },
+    ],
+    options: {
+      color: ["#181ee8", "#181ee8"],
+      plotOptions: {
+        radius: 30,
+      },
+      chart: {
+        background: "transparent",
+        foreColor: "#d0d2d6",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        show: true,
+        curve: ["smooth", "straight", "stepline"],
+        lineCap: "butt",
+        colors: "#f0f0f0",
+        width: 0.5,
+        dashArray: 0,
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apl", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      },
+      legend: {
+        position: "top",
+      },
+      responsive: [
+        {
+          breakpoint: 565,
+          yaxis: {
+            categories: ["Jan", "Feb", "Mar", "Apl", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          },
+          options: {
+            plotOptions: {
+              bar: {
+                horizontal: true,
+              },
+            },
+            chart: {
+              height: "550px",
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <div className='px-2 md:px-7 py-5'>
+      <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7'>
+        <div className='flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3'>
+          <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+            <h2 className='text-3xl font-bold'>$3456</h2>
+            <span className='font-medium'>Total Sale</span>
+          </div>
+          <div className='w-[40px] h-[47px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl'>
+            <MdCurrencyExchange className='text-[#fae8e8] shadow-lg' />
+          </div>
+        </div>
+
+        <div className='flex justify-between items-center p-5 bg-[#fde2ff] rounded-md gap-3'>
+          <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+            <h2 className='text-3xl font-bold'>50</h2>
+            <span className='font-medium'>Products</span>
+          </div>
+          <div className='w-[40px] h-[47px] rounded-full bg-[#760077] flex justify-center items-center text-xl'>
+            <MdProductionQuantityLimits className='text-[#fae8e8] shadow-lg' />
+          </div>
+        </div>
+
+        <div className='flex justify-between items-center p-5 bg-[#e9feea] rounded-md gap-3'>
+          <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+            <h2 className='text-3xl font-bold'>10</h2>
+            <span className='font-medium'>Sellers</span>
+          </div>
+          <div className='w-[40px] h-[47px] rounded-full bg-[#038000] flex justify-center items-center text-xl'>
+            <FaUser className='text-[#fae8e8] shadow-lg' />
+          </div>
+        </div>
+
+        <div className='flex justify-between items-center p-5 bg-[#ecebff] rounded-md gap-3'>
+          <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+            <h2 className='text-3xl font-bold'>43</h2>
+            <span className='font-medium'>Orders</span>
+          </div>
+          <div className='w-[40px] h-[47px] rounded-full bg-[#0200f8] flex justify-center items-center text-xl'>
+            <FaCartShopping className='text-[#fae8e8] shadow-lg' />
+          </div>
+        </div>
+      </div>
+
+      <div className='w-full flex flex-wrap mt-7'>
+        <div className='w-full lg:w-7/12 lg:pr-3'>
+          <div className='w-full bg-[#6a5fdf] p-4 rounded-md'>
+            <Chart options={state.options} series={state.series} type='bar' height={350} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
