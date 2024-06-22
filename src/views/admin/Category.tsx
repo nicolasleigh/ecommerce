1220151fc08be3ad8819fc49ce204bdbfc192b03
Nicolas Leigh <linze201@gmail.com>
2024-06-22 21:56:20 +0800

    create the add category field in the Category page

--- src/views/admin/Category.tsx
+++ src/views/admin/Category.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { FaEdit, FaTrash } from "react-icons/fa";
+import { FaEdit, FaImage, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Pagination from "../Pagination";
+import { IoMdCloseCircle } from "react-icons/io";
 
 export default function Category() {
   const [parPage, setParPage] = useState(5);
@@ -11,6 +12,15 @@ export default function Category() {
 
   return (
     <div className='px-2 lg:px-7 pt-5'>
+      <div className='flex lg:hidden justify-between items-center mb-5 p-4 bg-[#6a5fdf] rounded-md'>
+        <h1 className='text-[#d0d2d6] font-semibold text-lg'>Category</h1>
+        <button
+          onClick={() => setShow(true)}
+          className='bg-red-500 shadow-lg hover:shadow-red-500/40 px-4 py-2 cursor-pointer text-white rounded-sm text-sm'
+        >
+          Add
+        </button>
+      </div>
       <div className='flex flex-wrap w-full'>
         <div className='w-full lg:w-7/12'>
           <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
@@ -96,7 +106,52 @@ export default function Category() {
             </div>
           </div>
         </div>
-        <div className='w-full lg:w-5/12'></div>
+        <div
+          className={`w-[320px] lg:w-5/12 lg:relative lg:right-0 fixed ${
+            show ? "right-0" : "-right-[340px]"
+          } z-[100] top-0 transition-all duration-500`}
+        >
+          <div className='w-full pl-5'>
+            <div className='bg-[#6a5fdf] h-screen lg:h-auto px-3 py-2 lg:rounded-md text-[#d0d2d6]'>
+              <div className='flex justify-between items-center mb-4'>
+                <h1 className='text-[#d0d2d6] font-semibold text-xl w-full text-center'>Add Category</h1>
+                <div onClick={() => setShow(false)} className='block lg:hidden'>
+                  <IoMdCloseCircle />
+                </div>
+              </div>
+
+              <form>
+                <div className='flex flex-col w-full gap-1 mb-3'>
+                  <label htmlFor='name'>Category Name</label>
+                  <input
+                    type='text'
+                    id='name'
+                    name='category_name'
+                    placeholder='Category Name'
+                    className='px-4 py-2 focus:border-indigo-500 outline-none bg-transparent border border-slate-700 rounded-md text-[#d0d2d6]'
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor='image'
+                    className='flex justify-center items-center flex-col h-[238px] cursor-pointer border border-dashed hover:border-red-500 w-full border-[#d0d2d6]'
+                  >
+                    <span>
+                      <FaImage />
+                    </span>
+                    <span>Select Image</span>
+                  </label>
+                  <input type='file' name='image' id='image' className='hidden' />
+                  <div>
+                    <button className='bg-red-500 w-full hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2'>
+                      Add Category
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
