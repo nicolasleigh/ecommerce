e5db79ad082f8c44cb4ef463a5038d182b4741f8
Nicolas Leigh <linze201@gmail.com>
2024-06-28 20:34:38 +0800

    set seller details and seller status

--- src/store/reducers/sellerReducer.ts
+++ src/store/reducers/sellerReducer.ts
@@ -8,7 +8,36 @@ export const getSellerRequest = createAsyncThunk(
       const { data } = await api.get(`/request-seller-get?page=${page}&searchValue=${searchValue}&parPage=${parPage}`, {
         withCredentials: true,
       });
+      // console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const getSeller = createAsyncThunk(
+  "seller/getSeller",
+  async (sellerId, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get(`/get-seller/${sellerId}`, {
+        withCredentials: true,
+      });
       console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const sellerStatusUpdate = createAsyncThunk(
+  "seller/sellerStatusUpdate",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post(`/seller-status-update`, info, {
+        withCredentials: true,
+      });
       return fulfillWithValue(data);
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -24,6 +53,7 @@ export const sellerReducer = createSlice({
     loader: false,
     sellers: [],
     totalSeller: 0,
+    seller: "",
   },
   reducers: {
     messageClear: (state, _) => {
@@ -31,10 +61,18 @@ export const sellerReducer = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(getSellerRequest.fulfilled, (state, { payload }) => {
-      state.sellers = payload.sellers;
-      state.totalSeller = payload.totalSeller;
-    });
+    builder
+      .addCase(getSellerRequest.fulfilled, (state, { payload }) => {
+        state.sellers = payload.sellers;
+        state.totalSeller = payload.totalSeller;
+      })
+      .addCase(getSeller.fulfilled, (state, { payload }) => {
+        state.seller = payload.seller;
+      })
+      .addCase(sellerStatusUpdate.fulfilled, (state, { payload }) => {
+        state.seller = payload.seller;
+        state.successMessage = payload.message;
+      });
   },
 });
 
--- src/views/admin/SellerDetails.tsx
+++ src/views/admin/SellerDetails.tsx
@@ -1,4 +1,42 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getSeller, messageClear, sellerStatusUpdate } from "../../store/reducers/sellerReducer";
+import toast from "react-hot-toast";
+
 export default function SellerDetails() {
+  const dispatch = useDispatch();
+  const { seller, successMessage } = useSelector((state) => state.seller);
+  const { sellerId } = useParams();
+
+  const [status, setStatus] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      sellerStatusUpdate({
+        sellerId,
+        status,
+      })
+    );
+  };
+
+  useEffect(() => {
+    dispatch(getSeller(sellerId));
+  }, [sellerId]);
+
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
+    }
+  }, [successMessage]);
+
+  useEffect(() => {
+    if (seller) {
+      setStatus(seller.status);
+    }
+  }, [seller]);
+
   return (
     <div className='px-2 lg:px-7 pt-5'>
       <h1 className='text-lg font-bold mb-3'>Seller Detail</h1>
@@ -6,7 +44,11 @@ export default function SellerDetails() {
         <div className='w-full flex flex-wrap text-[#d0d2d6]'>
           <div className='w-3/12 flex justify-center items-center py-3'>
             <div>
-              <img src='http://localhost:5173/demo.jpg' alt='seller image' className='w-full h-[230px]' />
+              {seller.image ? (
+                <img src={seller.image} alt='seller image' className='w-full h-[230px]' />
+              ) : (
+                <span>Image Not Uploaded</span>
+              )}
             </div>
           </div>
 
@@ -18,23 +60,23 @@ export default function SellerDetails() {
               <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-[#9e97e9] rounded-md'>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>Name: </span>
-                  <span>Tim Bob</span>
+                  <span>{seller.name}</span>
                 </div>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>Email: </span>
-                  <span>tim@e.com</span>
+                  <span>{seller.email}</span>
                 </div>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>Role: </span>
-                  <span>Seller</span>
+                  <span>{seller.role}</span>
                 </div>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>Status: </span>
-                  <span>Active</span>
+                  <span>{seller.status}</span>
                 </div>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>Payment Status: </span>
-                  <span>Active</span>
+                  <span>{seller.payment}</span>
                 </div>
               </div>
             </div>
@@ -48,28 +90,31 @@ export default function SellerDetails() {
               <div className='flex justify-between text-sm flex-col gap-2 p-4 bg-[#9e97e9] rounded-md'>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>Shop Name: </span>
-                  <span>Easy Shop</span>
+                  <span>{seller.shopInfo?.shopName}</span>
                 </div>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>Division: </span>
-                  <span>Dhaka</span>
+                  <span>{seller.shopInfo?.division}</span>
                 </div>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>District: </span>
-                  <span>Dhaka</span>
+                  <span>{seller.shopInfo?.district}</span>
                 </div>
                 <div className='flex gap-2 font-bold text-black'>
                   <span>State: </span>
-                  <span>Palton</span>
+                  <span>{seller.shopInfo?.subDistrict}</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='flex gap-4 py-3'>
               <select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                required
                 name=''
                 id=''
                 className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]'
