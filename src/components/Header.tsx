4bd0980b7e40a038435e87aba50893f7a08a8e4e
Nicolas Leigh <linze201@gmail.com>
2024-06-28 22:04:11 +0800

    add images into public folder; create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,81 @@
+import { FaFacebookF, FaGithub, FaLinkedin, FaLock, FaTwitter, FaUser } from "react-icons/fa";
+import { IoMdArrowDropdown, IoMdPhonePortrait } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  const user = true;
+  return (
+    <div className='w-full bg-white'>
+      <div className='header-top bg-[#caddff] md-lg:hidden'>
+        <div className='w-[85%] lg:w-[90%] mx-auto'>
+          <div className='flex w-full justify-between items-center h-[50px] text-slate-500'>
+            <ul className='flex justify-start items-center gap-8 font-semibold text-black'>
+              <li className='flex relative justify-center items-center gap-2 text-sm after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px]'>
+                <span>
+                  <MdEmail />
+                </span>
+                <span>suport@e.com</span>
+              </li>
+              <li className='flex relative justify-center items-center gap-2 text-sm after:absolute '>
+                <span>
+                  <IoMdPhonePortrait />
+                </span>
+                <span>+(123) 1423 452 </span>
+              </li>
+            </ul>
+            <div>
+              <div className='flex justify-center items-center gap-10'>
+                <div className='flex justify-center items-center gap-4 text-black'>
+                  <a href='#'>
+                    <FaFacebookF />
+                  </a>
+                  <a href='#'>
+                    <FaTwitter />
+                  </a>
+                  <a href='#'>
+                    <FaLinkedin />
+                  </a>
+                  <a href='#'>
+                    <FaGithub />
+                  </a>
+                </div>
+                <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px] '>
+                  <img src='/images/language.png' alt='language icon' />
+                  <span>
+                    <IoMdArrowDropdown />
+                  </span>
+                  <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
+                    <li>Chinese</li>
+                    <li>English</li>
+                  </ul>
+                </div>
+                {user ? (
+                  <Link
+                    className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black'
+                    to='/dashboard'
+                  >
+                    <span>
+                      <FaUser />
+                    </span>
+                    <span>Nicolas Leigh</span>
+                  </Link>
+                ) : (
+                  <Link
+                    className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black'
+                    to='/login'
+                  >
+                    <span>
+                      <FaLock />
+                    </span>
+                    <span>Login</span>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
