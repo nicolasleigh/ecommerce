4eac664075de355ef4d6473c318cf83dec0b6251
Nicolas Leigh <linze201@gmail.com>
2024-06-29 21:02:12 +0800

    create Banner and Category carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import Banner from "../components/Banner";
+import Category from "../components/Category";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className='w-full'>
       <Header />
+      <Banner />
+      <Category />
     </div>
   );
 }
