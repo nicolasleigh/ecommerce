8fb4ab20741552ac469bce2a8a457a66b56a7dac
Nicolas Leigh <linze201@gmail.com>
2024-07-01 20:27:59 +0800

    design register and login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import Shop from "./pages/Shop";
 import Card from "./pages/Card";
 import Shipping from "./pages/Shipping";
 import Details from "./pages/Details";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/card' element={<Card />} />
         <Route path='/shipping' element={<Shipping />} />
