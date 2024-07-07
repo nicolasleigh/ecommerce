f1177e21e41988286476e01eb94ca17e52f48394
Nicolas Leigh <linze201@gmail.com>
2024-07-07 16:46:48 +0800

    creating customer to seller chat system

---
+++ src/store/reducers/chatReducer.ts
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api";
+
+export const add_friend = createAsyncThunk("chat/add_friend", async (info, { rejectWithValue, fulfillWithValue }) => {
+  try {
+    const { data } = await api.post("/chat/customer/add-customer-friend", info);
+    // console.log(data);
+    return fulfillWithValue(data);
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+});
+export const send_message = createAsyncThunk(
+  "chat/send_message",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post("/chat/customer/send-message-to-seller", info);
+      // console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const chatReducer = createSlice({
+  name: "chat",
+  initialState: {
+    myFriends: [],
+    friendMsg: [],
+    currentFriend: "",
+    errorMessage: "",
+    successMessage: "",
+  },
+  reducers: {
+    messageClear: (state, _) => {
+      state.errorMessage = "";
+      state.successMessage = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(add_friend.fulfilled, (state, { payload }) => {
+        state.friendMsg = payload.messages;
+        state.currentFriend = payload.currentFriend;
+        state.myFriends = payload.myFriends;
+      })
+      .addCase(send_message.fulfilled, (state, { payload }) => {
+        let tempFriends = state.myFriends;
+        let index = tempFriends.findIndex((f) => f.friendId === payload.message.receiverId);
+        while (index > 0) {
+          let temp = tempFriends[index];
+          tempFriends[index] = tempFriends[index - 1];
+          tempFriends[index - 1] = temp;
+          index--;
+        }
+        state.myFriends = tempFriends
+        state.friendMsg = [...state.friendMsg, payload.message];
+        state.successMessage = 'Message send successfully'
+      });
+  },
+});
+
+export const { messageClear } = chatReducer.actions;
+export default chatReducer.reducer;
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,5 +1,6 @@
 import authReducer from "./reducers/authReducer";
 import cardReducer from "./reducers/cardReducer";
+import chatReducer from "./reducers/chatReducer";
 import dashboardReducer from "./reducers/dashboardReducer";
 import homeReducer from "./reducers/homeReducer";
 import orderReducer from "./reducers/orderReducer";
@@ -10,6 +11,7 @@ const rootReducer = {
   card: cardReducer,
   order: orderReducer,
   dashboard: dashboardReducer,
+  chat: chatReducer,
 };
 
 export default rootReducer;
